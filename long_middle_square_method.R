0bee3ecb4488844e549275fdca1a3e8a768d8955
Fatma <123814832+fatmaakpunar@users.noreply.github.com>
2026-02-21 20:43:32 +0300

    Update long_middle_square_method.R

--- long_middle_square_method.R
+++ long_middle_square_method.R
@@ -1,10 +1,10 @@
-n <- 400
-seed <- 50687108
+n <- 400 # üretilecek sayı adedi 319 da 111 in tekrarı dönüyor
+seed <- 50687108 # 8 basamaklı seed
 u <- numeric(n)
 
 for (i in 1:n){
-seed <- seed^2
-seed <- as.numeric(substr(sprintf("%016.0f", a),5,12)
-u[i] <- seed/10^8
+ seed <- seed^2
+ seed <- as.numeric(substr(sprintf("%016.0f", a),5,12)
+ u[i] <- seed/10^8
 }
 print(u)
